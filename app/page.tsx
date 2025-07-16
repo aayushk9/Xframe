4e23d71676ea14cff26b386dba75d3ee8bdc4b36
Aayush <ayushkokate1@gmail.com>
2025-07-16 17:43:53 +0530

    ref: minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+import { Images } from "@/components/Images";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Xframe: Your go to place for cool images 
+      <h1>
+        Xframe: Your go to place for cool images
+      </h1>
+      <div>
+        <h3>Images collection</h3>
+        <div>
+          <Images/>
+        </div>
+      </div>
     </div>
   );
-}
+} 
---
+++ components/Images.tsx
@@ -0,0 +1,8 @@
+export function Images () {
+    return (
+        <div>
+           <p>collector pack 1</p>
+           <p>collector pack 2</p> 
+        </div>
+    )
+}
